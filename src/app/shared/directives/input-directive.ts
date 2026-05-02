3a9e73fa511a06dc97dccf6dea93bb8056e76953
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-02 07:30:30 +0200

    Liaison de propriété avec signal et injection

--- src/app/shared/directives/input-directive.ts
+++ src/app/shared/directives/input-directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, signal } from '@angular/core';
 
 @Directive({
   selector: '[appInput]',
+  host: {
+    // '[style.color]': '"blue"',
+    // '[style.color]': 'getColor()',
+    '[style.color]': 'color()',
+  },
 })
 export class InputDirective {
+  myhost = inject(ElementRef<HTMLInputElement>);
+  color = signal<string>('purple');
+
   constructor() {
     console.log('in directive');
+    console.log(this.myhost);
+    // this.myhost.nativeElement.style.color = 'crimson';
+
+    setTimeout(() => {
+      this.color.set('green');
+    }, 3000);
+  }
+
+  getColor() {
+    return 'orange';
   }
 }
