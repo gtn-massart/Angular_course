96d0b20604b3243ed10a051c5266c2b63bb5db27
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-10 07:26:18 +0200

    import ReactiveFormsModule

--- src/app/formulary.ts
+++ src/app/formulary.ts
@@ -1,32 +1,40 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulary',
-  imports: [],
+  // ↓ ReacitveFormsModule doit être importer pour activer les fonctionnalités des formulaires réactifs.
+  //   Sans ce module, les directives comme [formGroup] ou formaControlName ne fonctionneront pas.
+  imports: [ReactiveFormsModule, JsonPipe],
   template: `
     <h1>Les formulaires</h1>
     <div class="contain">
       <div class="w-half">
-        <form class="flex flex-col mb-10">
+        <form [formGroup]="userForm" (submit)="submit()" class="flex flex-col mb-10">
           <div class="flex flex-col mb-10">
             <label for="lastname">Nom</label>
-            <input type="text" id="lastname">
+            <input formControlName="lastname" type="text" id="lastname" />
           </div>
           <div class="flex flex-col mb-10">
             <label for="firstname">Prénom</label>
-            <input type="text" id="firstname">
+            <input formControlName="firstname" type="text" id="firstname" />
           </div>
           <div class="flex flex-col mb-10">
             <label for="email">Email</label>
-            <input type="text" id="email">
+            <input formControlName="email" type="text" id="email" />
           </div>
           <div class="flex flex-col mb-20">
             <label for="password">Mot de passe</label>
-            <input type="password" id="password">
+            <input formControlName="password" type="password" id="password" />
           </div>
           <button class="btn btn-primary">Sauvegarder</button>
         </form>
       </div>
+      <pre class="w-half">
+        {{ userForm.value | json }}
+      </pre
+      >
     </div>
   `,
   styles: `
@@ -43,4 +51,15 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export class Formulary {}
+export class Formulary {
+  userForm = new FormGroup({
+    lastname: new FormControl('', { nonNullable: true }),
+    firstname: new FormControl('', { nonNullable: true }),
+    email: new FormControl('', { nonNullable: true }),
+    password: new FormControl('', { nonNullable: true }),
+  });
+
+  submit() {
+    console.log(this.userForm.value);
+  }
+}
